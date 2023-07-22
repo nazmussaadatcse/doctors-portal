8c973c8a2d34e33b4e43064027bf19f4eb496cb6
nazmussaadatcse <nazmus.bd97@gmail.com>
2023-07-22 18:09:27 +0600

    RegExp and form validation,signup

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -43,12 +43,14 @@ const SignUp = () => {
                         </label>
                         <input type='password' {...register('password',{
                             required:'Password is Required',
-                            minLength:{value:6,message:'Must be 6 Character'}
+                            minLength:{value:6,message:'Must be 6 Character'},
+                            pattern:{value:/(?=.*?[#?!@$%^&*-])/,message:'At least one special character'}
+                            // regular Exp 
                         })} className="input input-bordered w-full max-w-xs" />
                         {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
                     </div>
 
-                    <input className='btn btn-accent w-full' value='SignUp' type="submit" />
+                    <input className='btn btn-accent w-full mt-5' value='SignUp' type="submit" />
                 </form>
                 <p>Already have an account? <Link className='text-secondary' to='/login'>Please Login</Link></p>
                 <div className='divider'>OR</div>
